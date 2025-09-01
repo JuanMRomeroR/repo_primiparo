26be1e3b5fb0dc436056730accd7913f0913f834
Juan Romero <juanmromeror@udistrital.edu.co>
2025-09-01 15:58:37 -0500

    Mapa

--- programa.js
+++ programa.js
@@ -1,6 +1,8 @@
-var map = L.map('map').setView([4.6130845568829235, -74.15656299018546], 13);
+var map = L.map('map').setView([4.62804362941054, -74.065816584317239], 13);
+
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
     attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
 }).addTo(map);
-var marker = L.marker([4.6130845568829235, -74.15656299018546]).addTo(map);
+
+var marker = L.marker([4.62804362941054, -74.06581658431723]).addTo(map);
